caddcf1ed63e1ee4b3a230c6a892f78208e4037a
mananwalia959 <mananwalia959@gmail.com>
2019-02-21 10:04:49 +0530

    Reserve checkpoint

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../services/data.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  users:Object;
+  constructor(private dataservice:DataService) { }
+
+  ngOnInit() {
+    this.dataservice.getUsers().subscribe(data => {
+      this.users = data
+      console.log(this.users)
+    })
+  }
+  
+
+}
